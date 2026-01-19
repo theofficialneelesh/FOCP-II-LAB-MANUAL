12743914c31f53c2d72dc4a141ccd6231fc6d5b0
NEELESH NAGPAL <neelesh25csu161@ncuindia.edu>
2026-01-19 10:20:24 +0530

    Add files via upload

---
+++ operations.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+    int a;
+    int b;
+    cout<<"enter a";
+    cin >> a;
+    cout<<"enter b ";
+    cin >> b;
+    
+    int sub = a-b;
+    int prod = a*b;
+    int div = a/b;
+
+
+    cout<< "diffrence is :" << sub ;
+    cout<<"\n";
+    cout<< "product is : " << prod;
+    cout<<"\n";
+    cout<< "division is : " << div;
+
+
+
+
+}
